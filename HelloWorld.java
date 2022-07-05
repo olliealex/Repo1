f20c4642c4ae3fd01abc9849fc67a7b204cc76ea
olliealex <ollieoleksandr@gmail.com>
2022-07-05 02:32:13 -0700

    Adding HelloWorld.class and HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,5 @@
-package Section1;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Alex!");
-
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        int mySecondNumber = 12;
-        int myThirdNumber = myFirstNumber * 2;
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-        int myLastOne = 1000 - myTotal;
-        System.out.println(myTotal);
-        System.out.println(myLastOne);
     }
 }
